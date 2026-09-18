e3b8d9f82fafeebf3ee83d3d4238da24312e3916
Julian De Angelis <julian.deangelis@gmail.com>
2026-09-17 20:43:13 -0400

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.cs
@@ -0,0 +1,46 @@
+public class Solution {
+    public bool IsValid(string s) {
+        
+        Dictionary<char, char> map = new Dictionary<char, char>();
+        map.Add(']','[');
+        map.Add(')','(');
+        map.Add('}','{');
+
+        Stack<char> brackets = new Stack<char>();
+
+        for (int i = 0; i < s.Length; i++) {
+            if (!map.ContainsKey(s[i])) {
+                // add opening bracket to the stack
+                // Console.WriteLine("pushing: " + s[i]);
+                brackets.Push(s[i]);
+
+                // go to next character
+                continue;
+            }
+            if (map.ContainsKey(s[i])) {
+                // pop from the stack and confirm they are the matching pair
+                if (brackets.Count() > 0) {
+
+                char opening = brackets.Pop();
+                if (map[s[i]] == opening) {
+                    // this is good, continue
+                    continue;
+                }
+                else {
+                    return false;
+                }
+
+
+                }
+                else return false;
+
+            }
+        }
+
+        if (brackets.Count() > 0)
+            return false;
+
+        return true;
+
+    }
+}
